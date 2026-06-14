5f776021c3b14066434faa2a769d4aa4a805cda7
nicollasciuldin <nicollasfaro@hotmail.com>
2026-06-14 10:32:03 -0700

    ajustes Guideline 4 e Guideline 2.1(b) e versão 39

--- ios/RunLiveActivity/RunLiveActivity.swift
+++ ios/RunLiveActivity/RunLiveActivity.swift
@@ -2,44 +2,38 @@ import ActivityKit
 import WidgetKit
 import SwiftUI
 
-// Estrutura de dados que o Flutter vai enviar
-struct RunAttributes: ActivityAttributes {
+public struct LiveActivitiesAppAttributes: ActivityAttributes {
+
     public struct ContentState: Codable, Hashable {
-        var distance: String
-        var pace: String
-        var time: String
+        var emoji: String
     }
 }
 
 struct RunLiveActivity: Widget {
+
     var body: some WidgetConfiguration {
-        ActivityConfiguration(for: RunAttributes.self) { context in
-            // Layout da Tela de Bloqueio
-            HStack {
-                VStack(alignment: .leading) {
-                    Text("Corrida em Andamento").font(.caption).foregroundColor(.orange)
-                    Text(context.state.distance).font(.title2).bold()
-                }
-                Spacer()
-                VStack(alignment: .trailing) {
-                    Text("Ritmo").font(.caption)
-                    Text(context.state.pace).font(.title3)
-                }
-            }
-            .padding()
+
+        ActivityConfiguration(
+            for: LiveActivitiesAppAttributes.self
+        ) { context in
+
+            Text(context.state.emoji)
+
         } dynamicIsland: { context in
-            // Layout da Dynamic Island (Expandida e Compacta)
+
             DynamicIsland {
-                DynamicIslandExpandedRegion(.leading) { Text(context.state.distance) }
-                DynamicIslandExpandedRegion(.trailing) { Text(context.state.time) }
+
+                DynamicIslandExpandedRegion(.center) {
+                    Text(context.state.emoji)
+                }
+
             } compactLeading: {
-                Text("🏃")
+                Text("😀")
             } compactTrailing: {
-                Text(context.state.distance)
+                Text("😀")
             } minimal: {
-                Text("🏃")
+                Text("😀")
             }
         }
     }
 }
-
